8450441edfc5d8f97329578ae3f1636b9f5c8cbd
Joan Killeen <joan.killeen@bluewin.ch>
2026-01-04 15:27:18 +0100

    Fixed You Tube test

--- tests/test-1.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
- page.goto('https://www.youtube.com/');
- //await page.getByRole('button', { name: 'Accept the use of cookies and' }).click();
-
-  
-});
--- tests/youtube-playlist.spec.js
+++ tests/youtube-playlist.spec.js
@@ -3,8 +3,11 @@
 const { test, expect } = require('@playwright/test');
 
 test('Validate Youtube playlist', async( {page} )=>{
-    await page.goto('https://www.youtube.com');
-    
-    await page.getByRole('combobox', { name: 'Search' }).click();
+   page.goto('https://www.youtube.com/');
+
+  //await expect(page.getByRole('button', { name: 'Accept the use of cookies and' })).toBeVisible();
+
+  page.getByRole('button', { name: 'Accept the use of cookies and' }).click();
+  await page.getByRole('combobox', { name: 'Search' }).click();
 
 })
