915c2e034d18e4d459cef65f8447ca8ec46b627b
Joan Killeen <joan.killeen@bluewin.ch>
2026-01-04 14:09:19 +0100

    Fixed workflow

--- tests/record-at-cursor.spec.js
+++ tests/record-at-cursor.spec.js
@@ -1,12 +1,16 @@
-await page.getByRole('main').click();import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('BakkappaN bad login', async ({ page }) => {
+test('BakkappaN visibility of login button', async ({ page }) => {
   await page.goto('https://github.com/BakkappaN');
   await page.getByRole('link', { name: 'Sign in' }).click();
   await page.getByRole('textbox', { name: 'Username or email address' }).click();
   await page.getByRole('textbox', { name: 'Username or email address' }).fill('lkjhkjk');
   await page.getByRole('textbox', { name: 'Username or email address' }).press('Tab');
   await page.getByRole('textbox', { name: 'Password' }).fill('kjgjhghjgjg');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('assdfg');
+  await page.getByRole('main').click();
+  await expect(page.getByRole('button', { name: 'Sign in', exact: true })).toBeVisible();
   await page.getByRole('button', { name: 'Sign in', exact: true }).click();
   await expect(page.getByRole('alert')).toContainText('Incorrect username or password.');
 });
