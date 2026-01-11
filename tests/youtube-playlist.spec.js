15fcdea92f367bbe030b28da92d984f161f77a8e
Joan Killeen <joan.killeen@bluewin.ch>
2026-01-11 13:56:13 +0100

    complete youtube test

--- tests/youtube-playlist.spec.js
+++ tests/youtube-playlist.spec.js
@@ -3,11 +3,23 @@
 const { test, expect } = require('@playwright/test');
 
 test('Validate Youtube playlist', async( {page} )=>{
-   page.goto('https://www.youtube.com/');
+  //Open Youtube
+  page.goto('https://www.youtube.com/');
 
-  //await expect(page.getByRole('button', { name: 'Accept the use of cookies and' })).toBeVisible();
-
+  //Accept cookies
+  await expect(page.getByRole('button', { name: 'Accept the use of cookies and' })).toBeVisible();
   page.getByRole('button', { name: 'Accept the use of cookies and' }).click();
-  await page.getByRole('combobox', { name: 'Search' }).click();
 
+  //Enter Search text and search
+  await page.getByRole('combobox', { name: 'Search' }).click();
+  await page.getByRole('combobox', { name: 'Search' }).fill('cypress by testers talk');
+  await expect(page.getByRole('button', { name: 'Search', exact: true })).toBeEnabled();
+  await page.getByRole('button', { name: 'Search', exact: true }).click();
+
+  //Verify search results
+  await expect(page.getByRole('link', { name: 'Cypress by Testers Talk' })).toBeVisible();
+
+  //Open Link and verify
+  await page.getByRole('link', { name: 'Cypress by Testers Talk' }).click();
+  await expect(page).toHaveTitle(/.*Tutorial.*/);
 })
